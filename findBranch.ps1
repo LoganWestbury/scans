f9adf2ca79e31084f8f53a0d365a2e31bc755510
LoganWestbury <logan.westbury@gmail.com>
2018-01-11 13:24:19 +0000

    opens the site

--- findBranch.ps1
+++ findBranch.ps1
@@ -1,8 +1,21 @@
 Add-Type -AssemblyName Microsoft.VisualBasic 
 cls
-$orderID = "" 
+$orderID = "6363171" 
 $smoURL = "http://sales.arnoldclark.co.uk/SalesmanOffice2/itadmin/amendordercfudetails.aspx"
 
-$orderID = Read-Host("Enter the order ID: ")
+#$orderID = Read-Host("Enter the order ID: ")
 
 Write-Host ("ID = $orderID")
+
+
+$ie = new-object -ComObject "InternetExplorer.Application"
+
+
+$requestUri = $smoURL
+$passwordIdFragment = "passwordText";
+$buttonIdFragment = "logInButton";
+
+$ie.visible = $true
+#$ie.silent = $true
+$ie.navigate($requestUri)
+while($ie.Busy) { Start-Sleep -Milliseconds 100 }
