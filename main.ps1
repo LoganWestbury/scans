8153910ed9c3392a1c0708da8976d81df4f6db53
LoganWestbury <logan.westbury@gmail.com>
2018-01-11 14:55:06 +0000

    trying to update the output

--- main.ps1
+++ main.ps1
@@ -5,18 +5,36 @@ $items = Get-ChildItem -Recurse $folderpath *.pdf
 $orderID = "" 
 $counterID = 0
 $amountOfScans = $items.Length
+$typeOfScanner = ""
 
 foreach( $i in $items)  {
     Start-Process ((Resolve-Path ("$folderpath$i")).Path)
     Start-Sleep -Seconds 1
     $counterID++ 
-    $orderID = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the Order ID for scan $counterID / $amountOfScans :", $i) 
+    $orderID = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the Order ID for scan $counterID / $amountOfScans :", $i)
+    
+    if ($i.Length -eq 20){
+        $typeOfScanner = "Xerox"
+    }
+    elseif ($ie.Length -eq 29) {
+        $typeOfScanner = "Lexmark"
+    }
+    
+    $DateCreated = (((Resolve-Path ("$folderpath$i")).Path).CreationTime)
+    $DateDeleted = Get-Date 
+    #$FileSize = (Get-Item ((Resolve-Path ("$folderpath$i")).Path)).length/1KB
+    #$FileSize.length/1KB
+  
     Stop-Process -Name "Acro*"
+
     if ($orderID -eq "delete"){
+        $reasonForDeletion = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a reason for marking $i for deletion :", $i)
         Add-Content -Path 'C:\Scans\Landesk.Csv' -Value "`"$i`",`"delete`""
+        Add-Content -Path 'C:\Scans\deleted_stats.csv' -Value  "`"$typeOfScanner`",`"$i`",`"delete`",`"$DateCreated`",`"$DateDeleted`",`"$FileSize`",`"$reasonForDeletion`""
     }
     else {
         Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"$orderID`""
+        Add-Content -Path 'C:\Scans\renamed_stats.csv' -Value  "`"$typeOfScanner`",`"$i`",`"$orderID`",`"$DateCreated`",`"$DateDeleted`",`"$FileSize`""
     } 
 }
 
