55ce927261588ae29a76911277369badfdb629c4
LoganWestbury <logan.westbury@gmail.com>
2018-01-11 12:46:22 +0000

    tidied up
    tidied up

--- main.ps1
+++ main.ps1
@@ -1,4 +1,3 @@
-#[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
 Add-Type -AssemblyName Microsoft.VisualBasic 
 
 $folderpath = '\\10.99.1.253\Pre_Rename\'
@@ -9,28 +8,16 @@ $amountOfScans = $items.Length
 
 foreach( $i in $items)  {
     Start-Process ((Resolve-Path ("$folderpath$i")).Path)
-
     Start-Sleep -Seconds 1
-    
     $counterID++ 
-
-    #$orderID = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
     $orderID = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the Order ID for scan $counterID / $amountOfScans :", $i) 
-
     Stop-Process -Name "Acro*"
-    
-    #Add-Content -Path 'C:\Scans\Landesk.csv' -Value "$i","$orderID"
-    #Add-Content -Path 'C:\Scans\Landesk.csv' -Value "$orderID"
-
     if ($orderID -eq "delete"){
         Add-Content -Path 'C:\Scans\Landesk.Csv' -Value "`"$i`",`"delete`""
     }
     else {
         Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"$orderID`""
     } 
-    
-
-    #Rename-Item $i.FullName ("$orderID.pdf")
 }
 
 $csv = Import-Csv C:\Scans\Landesk.csv
@@ -48,11 +35,5 @@ $csv | Foreach-Object {
             Rename-Item $oldfile ($folderpath + "\" + $_.new + ".pdf") -Force
             Write-Host ("Renaming $oldfile to $_")
         }
-        
     }
-
 }
-
-#Import-Csv C:\Scans\Landesk.csv | where {$_.new -ne "delete"} | Export-Csv C:\Scans\Landesk1.csv -notypeinfo -Force
-
-
