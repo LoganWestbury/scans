47a95a8498a91e03a159d12be06aa1727600b44d
LoganWestbury <logan.westbury@gmail.com>
2018-01-12 11:19:58 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -7,37 +7,74 @@ $counterID = 0
 $amountOfScans = $items.Length
 $typeOfScanner = ""
 
+$scanArray = @()
+$scanObject = New-Object System.Object
+
+
+
 foreach( $i in $items)  {
     Start-Process ((Resolve-Path ("$folderpath$i")).Path)
     Start-Sleep -Seconds 1
     $counterID++ 
+
+ 
+
     $orderID = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the Order ID for scan $counterID / $amountOfScans :", $i)
-    
-    if ($i.Length -eq 20){
-        $typeOfScanner = "Xerox"
-    }
-    elseif ($ie.Length -eq 29) {
-        $typeOfScanner = "Lexmark"
-    }
-    
-    $DateCreated = (((Resolve-Path ("$folderpath$i")).Path).CreationTime)
-    $DateDeleted = Get-Date 
-    #$FileSize = (Get-Item ((Resolve-Path ("$folderpath$i")).Path)).length/1KB
-    #$FileSize.length/1KB
-  
-    Stop-Process -Name "Acro*"
 
     if ($orderID -eq "delete"){
         $reasonForDeletion = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a reason for marking $i for deletion :", $i)
-        Add-Content -Path 'C:\Scans\Landesk.Csv' -Value "`"$i`",`"delete`""
-        Add-Content -Path 'C:\Scans\deleted_stats.csv' -Value  "`"$typeOfScanner`",`"$i`",`"delete`",`"$DateCreated`",`"$DateDeleted`",`"$FileSize`",`"$reasonForDeletion`""
+    }
+
+
+    if ($i.Length -eq 24){
+        $typeOfScanner = "Xerox"
+    }
+    elseif ($ie.Length -eq 33) {
+        $typeOfScanner = "Lexmark"
+    }
+
+    $DateCreated = Get-ItemProperty ((Resolve-Path ("$folderpath$i")).Path) | select -exp CreationTime
+    $DateModified1 = Get-Date -Format dd/MM/yyyy        
+    $DateModified2 = Get-Date -Format HH:mm:ss
+    $DateModifiedTotal = ("$DateModified1 $DateModified2")
+    
+    $scanObject[$counterID] | Add-Member -type NoteProperty -name TempName -Value "$i" 
+    $scanObject[$counterID] | Add-Member -type NoteProperty -name NewName -Value "$orderID"
+    $scanObject[$counterID] | Add-Member -type NoteProperty -name TypeOfScanner -Value "$typeOfScanner"
+    $scanObject[$counterID] | Add-Member -type NoteProperty -name DateCreated -Value "$DateCreated"
+    $scanObject[$counterID] | Add-Member -type NoteProperty -name DateModified -Value "$DateModifiedTotal"
+    $scanObject[$counterID] | Add-Member -type NoteProperty -name ReasonForDeletion -Value "$reasonForDeletion"
+
+    $scanArray += $scanObject[$counterID]
+
+
+   
+  
+    Stop-Process -Name "Acro*"
+
+
+
+
+
+
+    if ($orderID -eq "delete"){
+
+        Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"$orderID`""
+        #Add-Content -Path 'C:\Scans\deleted_stats.csv' -Value  "`"$typeOfScanner`",`"$i`",`"delete`",`"$DateCreated`",`"$DateDeleted`",`"$FileSize`",`"$reasonForDeletion`""
     }
     else {
-        Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"$orderID`""
-        Add-Content -Path 'C:\Scans\renamed_stats.csv' -Value  "`"$typeOfScanner`",`"$i`",`"$orderID`",`"$DateCreated`",`"$DateDeleted`",`"$FileSize`""
-    } 
-}
 
+
+        Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"$orderID`""
+       # Add-Content -Path 'C:\Scans\renamed_stats.csv' -Value  "`"$typeOfScanner`",`"$i`",`"$orderID`",`"$DateCreated`",`"$DateDeleted`",`"$FileSize`""
+    } 
+
+
+
+
+
+}
+ <#
 $csv = Import-Csv C:\Scans\Landesk.csv
 
 $csv | Foreach-Object { 
@@ -55,3 +92,6 @@ $csv | Foreach-Object {
         }
     }
 }
+#>
+
+$scanArray | export-csv C:\Scans\renamed_stats.csv -notypeinformation
