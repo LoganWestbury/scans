87edb934ac9fe2f7cd055c020ea567467f06b9d4
LoganWestbury <logan.westbury@gmail.com>
2018-01-12 14:53:22 +0000

    working version with logging to 3 csvs

--- main.ps1
+++ main.ps1
@@ -1,39 +1,4 @@
-Add-Type -AssemblyName Microsoft.VisualBasic 
-
-$folderpath = '\\10.99.1.253\Pre_Rename\'
-$items = Get-ChildItem -Recurse $folderpath *.pdf
-$orderID = "" 
-$counterID = 0
-$amountOfScans = $items.Length
-
-foreach( $i in $items)  {
-    Start-Process ((Resolve-Path ("$folderpath$i")).Path)
-    Start-Sleep -Seconds 1
-    $counterID++ 
-    $orderID = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the Order ID for scan $counterID / $amountOfScans :", $i) 
-    Stop-Process -Name "Acro*"
-    if ($orderID -eq "delete"){
-        Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"delete`""
-    }
-    else {
-        Add-Content -Path 'C:\Scans\Landesk.csv' -Value "`"$i`",`"$orderID`""
-    } 
-}
-
-$csv = Import-Csv C:\Scans\Landesk.csv
-
-$csv | Foreach-Object { 
-    $oldfile = $folderpath + "\" + $_.old
-    if (Test-Path $oldfile)
-    {   
-        if ($_.new -eq "delete"){
-            Remove-Item $oldfile
-            Write-Host ("Deleting $oldfile") 
+$SoftwareName = $_.Name.Replace('"','""');
+            $SoftwareVersion = $_.Version.Replace('"','""');
+            $Report += '"{0}","{1}","{2}"' -f $ComputerName, $SoftwareName, $SoftwareVersion;
         }
-        else
-        {
-            Rename-Item $oldfile ($folderpath + "\" + $_.new + ".pdf") -Force
-            Write-Host ("Renaming $oldfile to $_")
-        }
-    }
-}
